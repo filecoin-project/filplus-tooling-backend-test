3006040ff08f6af2810f81053e40b24c38485f30
esraa <jbesraa@gmail.com>
2023-08-10 15:36:29 +0300

    test action

--- issue-to-pr.sh
+++ issue-to-pr.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-echo $1
+curl --header "Content-Type: application/json" --request POST --data '{"application_id": "'"$1"'"}' https://fp-core.6omfj573u6naa.us-east-2.cs.amazonlightsail.com/application
 
-echo "Issue Number: $1"
