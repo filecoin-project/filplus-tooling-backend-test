82c3d3c7a3cf9f085b952a4e1a2ba9f720a41691
Charly López <clriesco@gmail.com>
2023-11-02 12:58:08 +0100

    Delete issue-to-pr.sh

--- .github/scripts/issue-to-pr.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-curl --header "Content-Type: application/json" --request POST --data '{"application_id": "'"$1"'"}' https://fp-core.6omfj573u6naa.us-east-2.cs.amazonlightsail.com/application
-
