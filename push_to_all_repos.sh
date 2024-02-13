dace6c643c414ba55f6d2ec15a323dbf53d075d9
kokal33 <vasilev.s@protonmail.com>
2024-02-13 13:53:18 +0100

    push to all repos script

---
+++ push_to_all_repos.sh
@@ -0,0 +1,24 @@
+REPOS=(
+    "git@github.com:keyko-io/test-phillip-second.git"
+    "git@github.com:keyko-io/alexander-the-test.git"
+    "git@github.com:keyko-io/great-test-library.git"
+)
+
+# Ensure that changes are ready to commit
+# You can modify this part as per your requirement
+git add .
+echo "Enter commit message: "
+read COMMIT_MESSAGE
+git commit -m "$COMMIT_MESSAGE"
+
+# Push to each repository
+for REPO in "${REPOS[@]}"
+do
+    echo "Pushing to $REPO"
+    git push "$REPO" main
+done
+
+echo "Pushed to all repositories."
+
+# Wait for a button press before closing
+read -p "Press any key to continue..."
